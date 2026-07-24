b83034d504c1422c275380ad3880eeb059dba4a4
Mohammad Javani <javani.judo@gmail.com>
2026-07-24 16:48:51 +0330

    Judo Mind v0.2 - Localization foundation added

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,37 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appName => 'Judo Mind';
+
+  @override
+  String get videoLibrary => 'Video Library';
+
+  @override
+  String get trainingVideo => 'Training Video';
+
+  @override
+  String get competitionAnalysis => 'Competition Analysis';
+
+  @override
+  String get techniqueQuiz => 'Technique Quiz';
+
+  @override
+  String get officialMedia => 'Official Media';
+
+  @override
+  String get kuzushi => 'Kuzushi';
+
+  @override
+  String get tsukuri => 'Tsukuri';
+
+  @override
+  String get kake => 'Kake';
+}
