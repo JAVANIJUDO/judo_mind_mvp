da5811b9e637c99144055b2d0509b725d15ee097
Mohammad Javani <javani.judo@gmail.com>
2026-07-24 17:20:11 +0330

    Judo Mind v0.3 - Localization setup and video launcher fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'core/theme/app_theme.dart';
 import 'core/routes/app_routes.dart';
+import 'l10n/app_localizations.dart';
 
 
 void main() {
@@ -23,20 +25,33 @@ class JudoMindApp extends StatelessWidget {
 
     return MaterialApp(
 
-
       debugShowCheckedModeBanner: false,
 
 
       title: "Judo Mind",
 
 
-
       theme: AppTheme.darkTheme,
 
 
+      // Localization
+      localizationsDelegates: const [
+
+        AppLocalizations.delegate,
+
+        GlobalMaterialLocalizations.delegate,
+
+        GlobalWidgetsLocalizations.delegate,
+
+        GlobalCupertinoLocalizations.delegate,
+
+      ],
+
+
+      supportedLocales: AppLocalizations.supportedLocales,
+
 
       initialRoute: AppRoutes.home,
-
 
 
       routes: AppRoutes.routes,
